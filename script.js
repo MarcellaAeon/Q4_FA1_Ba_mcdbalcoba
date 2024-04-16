3955cfc707602f169d3e09db950463103365d962
Martheena Balcoba <143463459+MarcellaAeon@users.noreply.github.com>
2024-04-17 00:39:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -17,71 +17,71 @@ function userCredentials(){
 }
 function noNameInput(){
     var bgcolor = document.getElementById("name");
-    var hex = "#A68585";
+    var hex = "2px solid #A68585";
     bgcolor.style.border = hex;
 }
 function noUserInput(){
     var bgcolor = document.getElementById("user");
-    var hex = "#A68585";
+    var hex = "2px solid #A68585";
     bgcolor.style.border = hex;
 }
 function noEmailInput(){
     var bgcolor = document.getElementById("email");
-    var hex = "#A68585";
+    var hex = "2px solid #A68585";
     bgcolor.style.border = hex;
 }
 function noPassInput(){
     var bgcolor = document.getElementById("pass");
-    var hex = "#A68585";
+    var hex = "2px solid #A68585";
     bgcolor.style.border = hex;
 }
 function noAddressInput(){
     var bgcolor = document.getElementById("adddress");
-    var hex = "#A68585";
+    var hex = "2px solid #A68585";
     bgcolor.style.border = hex;
 }
 function noGenderInput(){
     var bgcolor = document.getElementById("gender");
-    var hex = "#A68585";
+    var hex = "2px solid #A68585";
     bgcolor.style.border = hex;
 }
 function noInterestsInput(){
     var bgcolor = document.getElementById("interests");
-    var hex = "#A68585";
+    var hex = "2px solid #A68585";
     bgcolor.style.border = hex;
 }
 function hasNameInput(){
     var bgcolor = document.getElementById("name");
-    var hex = "#84A59D";
+    var hex = "2px solid #84A59D";
     bgcolor.style.border = hex;
 }
 function hasUserInput(){
     var bgcolor = document.getElementById("user");
-    var hex = "#84A59D";
+    var hex = "2px solid #84A59D";
     bgcolor.style.border = hex;
 }
 function hasEmailInput(){
     var bgcolor = document.getElementById("email");
-    var hex = "#84A59D";
+    var hex = "2px solid #84A59D";
     bgcolor.style.border = hex;
 }
 function hasPassInput(){
     var bgcolor = document.getElementById("pass");
-    var hex = "#84A59D";
+    var hex = "2px solid #84A59D";
     bgcolor.style.border = hex;
 }
 function hasAddressInput(){
     var bgcolor = document.getElementById("address");
-    var hex = "#84A59D";
+    var hex = "2px solid #84A59D";
     bgcolor.style.border = hex;
 }
 function hasGenderInput(){
     var bgcolor = document.getElementById("gender");
-    var hex = "#84A59D";
+    var hex = "2px solid #84A59D";
     bgcolor.style.border = hex;
 }
 function hasInterestsInput(){
     var bgcolor = document.getElementById("interests");
-    var hex = "#84A59D";
+    var hex = "2px solid #84A59D";
     bgcolor.style.border = hex;
 }
